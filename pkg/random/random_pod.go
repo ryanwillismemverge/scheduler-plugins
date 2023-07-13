42fac7c0ea452e1bb8761ea76f6455413233ae89
Ryan Willis <ryan.willis@memverge.com>
2023-07-13 16:08:14 +0000

    random number scheduler plugin

---
+++ pkg/random/random_pod.go
@@ -0,0 +1,38 @@
+package randompod
+
+import (
+	"context"
+	"math/rand"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/kubernetes/pkg/scheduler/framework"
+)
+
+
+type RandomPod struct(
+	handle framework.Handle
+)
+
+// Name is the name of the plugin used in the Registry and configurations.
+const Name = "RandomPod"
+
+var _ = framework.ScorePlugin(&RandomPod)
+
+func (ps *RandomPod) Name() string {
+	return Name
+}
+
+func (ps *RandomPod) Score(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeName string) (int64, *framework.Status) {
+	score := rand.Int63n(100) + 1
+	return score, framework.NewStatus(framework.Success)
+}
+
+func (ps *RandomPod) NormalizeScore(ctx context.Context, state *framework.CycleState, pod *v1.Pod, scores framework.NodeScoreList) *framework.Status {
+	// If the scores are already in the range 1-100, there's no need to normalize them.
+	// So, just return a successful status.
+	return framework.NewStatus(framework.Success)
+}
+
+func New(_ runtime.Object, h framework.Handle) (framework.Plugin, error){
+	return &RandomPod{handle: h}, nil
+}
